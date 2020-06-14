44171eabb0f556e55a893d17404d6cf501b1a14c
deepaliattarde <66907213+deepaliattarde@users.noreply.github.com>
2020-06-14 19:28:49 +0530

    Add files via upload

---
+++ navigus/Navigus.java
@@ -0,0 +1,8 @@
+package navigus;
+public class Navigus {
+    public static void main(String[] args) {
+        // Set the home screen
+        Home h=new Home();
+        h.setVisible(true);
+    }
+}
